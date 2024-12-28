49aa3607098ad028fdec1667d63f006d528ab91b
mukild.22it@kongu.edu <mukild.22it@kongu.edu>
2024-12-28 11:30:07 +0530

    adding loop.js

---
+++ loop.js
@@ -0,0 +1,49 @@
+//array =[
+   // 2, 4,5,[40.6, 45.3],'a','true',"black"
+//]
+
+//console.log(array);
+//for(var a in array){
+ //   console.log(a);
+  //  console.log(array[a]);
+//}
+//for (var a of array){
+  //  console.log(a);
+//}
+//objtype1={
+ //    "name":"mukil",
+   //  "age":20
+//}
+//console.log(objtype1);
+//objtype={}
+//console.log(objtype);
+//objtype["movie"]='iron man 2'
+//objtype['value']='1000 cr'
+//console.log(objtype.movie);
+//console.log(objtype['movie']);
+//assingment operator
+var a=10;
+var b=10;
+var c=a+b;
+console.log(c);
+//arithmatic operator
+var d =2;
+var e=23;
+var f=d-e;
+console.log(f);
+//comparison operator 
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+console.log(result);
+
+var ac=10;
+var bc='10';
+if(a==b){
+    console.log(ac+"=="+bc)
+}
+else console.log(ac+"!=="+bc)
+
+
+
+
